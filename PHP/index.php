9708d96d5b25725c2914fba798755235a860b5cd
Tucker <twilkes149@gmail.com>
2017-12-07 22:48:18 -0700

    testing database

--- PHP/index.php
+++ PHP/index.php
@@ -1,11 +1,31 @@
 <?php
 	
 	require_once 'login.php';
-	echo "hello world";
+	echo "hello world\n";
+
+	
+
+	$dsn = "pgsql:"
+    . "host=".$hn.";"
+    . "dbname=".$db.";"
+    . "user=".$un.";"
+    . "port=".$port.";"
+    . "sslmode=require;"
+    . "password=".$pw.";";
+
+	$db = new PDO($dsn);
+	
 
 	$conn = new mysqli($hn, $db, $un, $pw);
 	if ($conn->connect_error)
-		echo "error connecting";
+		echo "error connecting: " . $conn->connect_error;
 	else
 		echo "connected";
+
+	try {
+    	$dbh = new PDO($dhn, $un, $pw);
+    	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch (PDOException $e) {
+	    echo 'Connection failed: ' . $e->getMessage();
+	}
 ?>
--- PHP/login.php
+++ PHP/login.php
@@ -3,3 +3,5 @@
 	$db = "dd5uv46slgjh5d";
 	$un = "ygjimiznfuhdwx";
 	$pw = "    43f3f9ce69ff11e6225e5796b4996eeb15878c95a6feb30e668ecb19dde68a3e";
+	$port = "5432";
+?>
