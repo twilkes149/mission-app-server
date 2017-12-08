99aef55ae6d49907cbc0f08487023e3c4dbf14d0
Tucker <twilkes149@gmail.com>
2017-12-07 22:39:27 -0700

    testing database

--- PHP/index.php
+++ PHP/index.php
@@ -1,4 +1,11 @@
 <?php
-
+	
+	require_once 'login.php';
 	echo "hello world";
+
+	$conn = new mysqli($hn, $db, $un, $pw);
+	if ($conn->connect_error)
+		echo "error connecting";
+	else
+		echo "connected";
 ?>
---
+++ PHP/login.php
@@ -0,0 +1,5 @@
+<?php
+	$hn = "ec2-54-235-76-111.compute-1.amazonaws.com";
+	$db = "dd5uv46slgjh5d";
+	$un = "ygjimiznfuhdwx";
+	$pw = "    43f3f9ce69ff11e6225e5796b4996eeb15878c95a6feb30e668ecb19dde68a3e";
