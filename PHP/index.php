1fa060903a55a542f91c097e8d0bfe7059382265
twilkes149 <twilkes149@gmail.com>
2017-12-10 16:29:19 -0700

    testing db

--- PHP/index.php
+++ PHP/index.php
@@ -9,6 +9,19 @@
     	$dbh = new PDO("mysql:dbname=$db;host=$hn", $un, $pw);
     	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+      //set up sql statement
+      $query = "CREATE TABLE if not exists user (
+        id INT NOT NULL AUTO_INCREMENT,
+        userName TEXT NOT NULL,
+        password TEXT NOT NULL);";
+
+      $result = $dbh->query($query);
+      
+      if ($result)
+        echo "success";
+      else
+        echo "failed";
+
 
 	} catch (PDOException $e) {
 	    echo '\nConnection failed: ' . $e->getMessage();
