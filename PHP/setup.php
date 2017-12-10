100976c1eb2f7d6e26fe07ab385bba4285423c90
Tucker <twilkes149@gmail.com>
2017-12-10 01:08:09 -0700

    started database setup script

---
+++ PHP/setup.php
@@ -0,0 +1,21 @@
+<?php
+	require_once "login.php";
+
+	$conn = new mysqli($hn, $un, $pw, $db);
+
+	//create database connection
+	if ($conn->connect_error
+		die($conn->connect_error);
+
+	//set up sql statement
+	$query = "CREATE TABLE if not exists user (
+		id INT NOT NULL AUTO_INCREMENT,
+		userName TEXT NOT NULL,
+		password TEXT NOT NULL);";
+
+	$result = $conn->query($query);//query the database
+	if (!$result)//error happened
+		die("Database query failed: " . $conn->error);
+	else
+		echo "Success.";
+?>
