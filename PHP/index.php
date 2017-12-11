0d97dc5a36f6854563d2669fc782c4faf8e7980e
twilkes149 <twilkes149@gmail.com>
2017-12-10 21:25:51 -0700

    db test

--- PHP/index.php
+++ PHP/index.php
@@ -8,8 +8,12 @@
   function pg_connection_string_from_database_url() {
     extract(parse_url($_ENV["DATABASE_URL"]));
 
-    echo $_ENV['DATABASE_URL'] . "\n";
-    
+    echo $_ENV['DATABASE_URL'] . "\n" . " userName: $user, password: $pass, ";
+
+    $dbopts = parse_url(getenv('DATABASE_URL'));
+    echo "dbopts: " . $dbopts;
+
+
     return "user=$user password=$pass host=$host dbname=" . substr($path, 1); // <- you may want to add sslmode=require there too
   }
   // Here we establish the connection. Yes, that's all.
