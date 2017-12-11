cc5b94e8d863d7d48a6a49f056dea05230138278
twilkes149 <twilkes149@gmail.com>
2017-12-10 21:12:25 -0700

    database testing

--- PHP/index.php
+++ PHP/index.php
@@ -3,27 +3,25 @@
 	require_once 'login.php';
 	echo "hello world\n";
 
-	$db = new PDO($dsn);
+	//This function reads your DATABASE_URL config var and returns a connection
+  //string suitable for pg_connect. Put this in your app.
+  function pg_connection_string_from_database_url() {
+    extract(parse_url($_ENV["DATABASE_URL"]));
+    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); // <- you may want to add sslmode=require there too
+  }
+  // Here we establish the connection. Yes, that's all.
+  $pg_conn = pg_connect(pg_connection_string_from_database_url());
+  
+  $query = "CREATE TABLE if not exists user (
+    id INT NOT NULL AUTO_INCREMENT,
+    userName TEXT NOT NULL,
+    password TEXT NOT NULL);";
 
-	try {
-    	$dbh = new PDO("mysql:dbname=$db;host=$hn", $un, $pw);
-    	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // Now let's use the connection for something silly just to prove it works:
+  $result = pg_query($pg_conn, $query);
 
-      //set up sql statement
-      $query = "CREATE TABLE if not exists user (
-        id INT NOT NULL AUTO_INCREMENT,
-        userName TEXT NOT NULL,
-        password TEXT NOT NULL);";
-
-      $result = $dbh->query($query);
-      
-      if ($result)
-        echo "success";
-      else
-        echo "failed";
-
-
-	} catch (PDOException $e) {
-	    echo '\nConnection failed: ' . $e->getMessage();
-	}
+  if ($result)
+    echo "success";
+  else
+    echo "failed";
 ?>
